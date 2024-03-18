328f63b916a0700ac46a3cd1bf5aeb7d0e967a85
Dennis Heine <dennis.heine@online.de>
2024-03-19 00:19:57 +0100

    Initial Commit

---
+++ Trailblazer/TrailblazerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailblazerApp.swift
+//  Trailblazer
+//
+//  Created by Dennis Heine on 19.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TrailblazerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
